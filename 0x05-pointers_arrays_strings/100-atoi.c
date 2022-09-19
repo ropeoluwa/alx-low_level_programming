6d83a15ad779d9c0213220573186e17439fa9fdd
ropeoluwa <ropeoluwa@hotmail.com>
2022-09-19 12:52:08 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,46 +1,34 @@
-#include "main.h"
-#include "2-strlen.c"
+#include <stdio.h>
 
 /**
- * _atoi - converts string to integer
- * @s: string to convert
- *
- * Return: returns integer value
+ * _atoi - a function that converts string to integer
+ * @s: An input string
+ * Return: integer from conversion
  */
 int _atoi(char *s)
 {
-	int i;
-	int np = 0;
-	int c;
-	int d = 1;
-	int num = 0;
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
 
-	for (i = 0; i < _strlen(s); i++)
+	while (*s)
 	{
-		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
-			break;
-		if (s[i] == '-')
-			np--;
-		if (s[i] == '+')
-			np++;
-		if (s[i] >= '0' && s[i] <= '9')
+		if (*s == '-')
+			sign *= -1;
+
+		if (*s >= '0' && *s <= '9')
 		{
-			c++;
+			null_flag = 1;
+			total = total * 10 + *s - '0';
 		}
+
+		else if (null_flag)
+			break;
+		s++;
 	}
-	while (c > 0)
-	{
-		num += ((s[i - 1] - '0') * d);
-		i--;
-		c--;
-		d *= 10;
-	}
-	if (np >= 0)
-	{
-		num *= 1;
-	} else
-	{
-		num *= -1;
-	}
-	return (num);
+
+	if (sign < 0)
+		total = (-total);
+
+	return (total);
 }
