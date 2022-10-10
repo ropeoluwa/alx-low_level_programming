2d6344b3c49b205f581418afde28b4be3dd4cede
ropeoluwa <ropeoluwa@hotmail.com>
2022-10-10 13:51:37 -0700

    dog

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,20 +1,21 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 /**
- * struct dog - structure of dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * struct dog - Defines a dogs attributes
+ * @name: A string for the dogs name
+ * @age: An integer for the dogs age
+ * @owner: A string for the owners name
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-typedef struct dog dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
---
+++ 0x0D-preprocessor/0x0E-structures_typedef/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * struct dog - Defines a dogs attributes
+ * @name: A string for the dogs name
+ * @age: An integer for the dogs age
+ * @owner: A string for the owners name
+ */
+typedef struct dog
