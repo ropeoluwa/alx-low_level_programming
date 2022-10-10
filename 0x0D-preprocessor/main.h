c5926e15507a69da890dff27fdf855634f584e0f
ropeoluwa <ropeoluwa@hotmail.com>
2022-10-10 13:14:55 -0700

    new

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * main file
+ * file: integer to compare
+ * Return: 0
+ */
+
+/**
+ * main file
+ * file: integer to compare
+ * Return: 0
+ */
+
+int main(void);
